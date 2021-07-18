a1932bb1c78a90dee3fa27896e22cc8f373fa21a
sunqiangqiang <sunqiangqiang@quicloud.cn>
2021-07-19 01:42:16 +0800

    update!

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   prefix: process.env.NODE_ENV === 'development' ? '' : '/h5Build',
-  host: process.env.NODE_ENV === 'development' ? 'http://localhost:3701' : 'https://cqmfe.club/page-builder-server'
+  host: process.env.NODE_ENV === 'development' ? 'http://localhost:3701' : 'http://1.15.148.243//page-builder-server'
 }
